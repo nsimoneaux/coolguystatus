84f2770bd5e36deea364814f176dffae4e27056e
nsimoneaux <nsimoneaux@gmail.com>
2016-05-09 03:05:47 -0600

    initial commit

--- cool_guy_status.rb
+++ cool_guy_status.rb
@@ -6,7 +6,7 @@ require "json"
 
 class CoolGuyStatus < Sinatra::Base
   liveURL = "http://stream.coolguyradio.com:80/live"
-  get '/status' do
+  get '/' do
     response = nil
     response = JSON.parse(Net::HTTP.get(URI('http://stream.coolguyradio.com/status-json.xsl')))
     if response["icestats"]["source"][0].length > 4
